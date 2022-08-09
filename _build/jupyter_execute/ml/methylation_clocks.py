95ef4717e2c00f9685bb3b932a119549260a717c
shappiron <fire-paladin@mail.ru>
2022-08-09 13:55:20 +0600

    test

---
+++ _build/jupyter_execute/ml/methylation_clocks.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Content with notebooks
+# 
+# You can also create content with Jupyter Notebooks. This means that you can include
+# code blocks and their outputs in your book.
+# 
+# ## Markdown + notebooks
+# 
+# As it is markdown, you can embed images, HTML, etc into your posts!
+# 
+# ![](https://myst-parser.readthedocs.io/en/latest/_static/logo-wide.svg)
+# 
+# You can also $add_{math}$ and
+# 
+# $$
+# math^{blocks}
+# $$
+# 
+# or
+# 
+# $$
+# \begin{aligned}
+# \mbox{mean} la_{tex} \\ \\
+# math blocks
+# \end{aligned}
+# $$
+# 
+# But make sure you \$Escape \$your \$dollar signs \$you want to keep!
+# 
+# ## MyST markdown
+# 
+# MyST markdown works in Jupyter Notebooks as well. For more information about MyST markdown, check
+# out [the MyST guide in Jupyter Book](https://jupyterbook.org/content/myst.html),
+# or see [the MyST markdown documentation](https://myst-parser.readthedocs.io/en/latest/).
+# 
+# ## Code blocks and outputs
+# 
+# Jupyter Book will also embed your code blocks and output in your book.
+# For example, here's some sample Matplotlib code:
+
+# In[1]:
+
+
+from matplotlib import rcParams, cycler
+import matplotlib.pyplot as plt
+import numpy as np
+plt.ion()
+
+
+# There is a lot more that you can do with outputs (such as including interactive outputs)
+# with your book. For more information about this, see [the Jupyter Book documentation](https://jupyterbook.org)
