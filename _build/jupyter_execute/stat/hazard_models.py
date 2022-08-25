35ed7626a3ed1d25ac195fd8fe914f7ea1083789
shappiron <fire-paladin@mail.ru>
2022-08-25 13:39:19 +0300

    added constant risk

--- _build/jupyter_execute/stat/hazard_models.py
+++ _build/jupyter_execute/stat/hazard_models.py
@@ -11,10 +11,12 @@
 # 
 # At this lesson we learn how to understand such mortality risk curves and much more. We will learn Gompertz model - the central conception in aging research.
 
-# ## Survival curves
+# ## Survival curve
 # 
 # Let $T$ is a non-negative random variable with a corresponding distribution function:
-# $$F_T(t) = P(T\leq t)$$ 
+# 
+# $$F(t) = P(T\leq t)$$ 
+# 
 # We call $T$ - *survival time* or time before death of some object (e.g. human). For example, $T$ can have normal distribution:
 
 # In[1]:
@@ -32,8 +34,8 @@ ax.set_ylabel('Probability to die')
 ax.plot(x, rv.cdf(x));
 
 
-# If $F_T(t)$ is a probability to die before time moment $T$ we can introduce a probability to survive before this moment $S$ as just a complement to $F$:
-# $$S_T(t) = P(T > t) = 1 - F_T(t)$$ 
+# If $F(t)$ is a probability to die before time moment $T$ we can introduce a probability to survive before this moment $S$ as just a complement to $F$:
+# $$S(t) = P(T > t) = 1 - F(t)$$ 
 # 
 # Let's draw it:
 
@@ -50,13 +52,49 @@ ax.plot(x, 1 - rv.cdf(x));
 
 # This plot is what we actually call *survival curve*. The intuition behind this is rather straightforward - it shows how many of experimental objects survived before a partucular moment of time. You can see a lot of empirical versions of such curves in articles. Rigorous analysis of this curves helps to avoid a misinterpretation of typical drug-testing experiment or other. And it's what we are going to do in further.
 
-# ## Hazard function
+# ## Mortality risk
 
-# In[ ]:
-
-
-
-
+# You could have heard that mortality risk increases exponentially with aging. But what does this actually mean?
+# 
+# ```{note}
+# :class: dropdown
+# Mortality risk is **not** a probability of death!
+# ```
+# 
+# Let's consider instantaneous probability of death that the object will die within a time interval $dt$ by condition that it already survived to moment $T$:
+# 
+# $$\lim_{dt \to 0}\frac{P(t < T ≤ t + dt | T > t)}{dt}$$
+# 
+# This conditional probability can be decomposed by definition as follows: 
+# 
+# $$P(A ∩ B) = P(A|B)P(B)$$
+# T
+# his yields:
+# 
+# $$P(t < T ≤ t + dt ∩ T > t) = P(t < T ≤ t + dt | T > t)P(T > t)$$
+# 
+# $$P(t < T ≤ t + dt | T > t) = \frac{P(t < T ≤ t + dt ∩ T > t)}{P(T > t)}$$
+# 
+# Recall that $P(T > t) = 1 - F(t) = S(t)$ and $P(t < T ≤ t + dt) = F(t+dt) - F(t)$.
+# 
+# ```{note}
+# :class: dropdown
+# To understand the last property it is enough to remember that probability function is just a square under probability density function!
+# ```
+# 
+# Now let's rewrite our result:
+# 
+# $$P(t < T ≤ t + dt | T > t) = \frac{F(t+dt) - F(t)}{S(t)}$$
+# 
+# It's time to put our derivation to the first formula with a limit. So, we have:
+# 
+# $$\lim_{dt \to 0}\frac{P(t < T ≤ t + dt | T > t)}{dt} = \lim_{dt \to 0}\frac{F(t+dt) - F(t)}{dt}\frac{1}{S(t)} = \frac{f(t)}{S(t)}$$
+# 
+# Yes, we obtained a ratio of probability density function of death and probability of survival. And this is what we call **mortality risk function** or hazard function.
+# 
+# $$m(t) = \frac{f(t)}{S(t)}$$
+# 
+# This is actually the thing that increases exponentially as we age and have a name of Gompertz law (quite more complex than just a probability, isn't it?). Now let's consider different kinds of such functions.
 
 # In[3]:
 
